83914bb045cce2f2bab4ee698aefd5c6864a6f66
yunha <k9779k@naver.com>
2020-06-01 04:11:47 +0900

    mysql port 변경

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host : '172.31.44.244',
-    port : 22,
+    port : 3306,
     user : 'root',
     password : 'mysql0113!',
     database : 'ajoutopia',
