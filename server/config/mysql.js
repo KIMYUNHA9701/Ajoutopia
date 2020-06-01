59ffd30acab0c5ffd8e47e439a2f37aa654f278d
yunha <k9779k@naver.com>
2020-06-01 18:30:26 +0900

    bodyparser

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host : '127.0.0.1',
-    port : 3306,
-    user : 'root',
-    password : 'ajoutopia',
-    database : 'Ajoutopia',
-    multipleStatements: true
-})
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : 'ajoutopia',
+      database : 'Ajoutopia',
+      multipleStatements: true
+  })
 
 module.exports = connection;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-  const app = require('./app')
+const app = require('./app')
 
   http.createServer(app).listen(5555,() => {
       console.log(`Backend Server is running on 5555..`)
